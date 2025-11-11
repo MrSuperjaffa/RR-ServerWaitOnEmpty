f30eb5d3efb798778f500ee8cd16963ebc68c71d
Jaffa <34530108+MrSuperjaffa@users.noreply.github.com>
2025-11-10 20:04:48 -0600

    Initial v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # RR-ServerWaitOnEmpty
 Enables Railroader to pause when their are no players connected.
+
+## Building
+Built in Visual Studio 2022
+
+Add `Railroader\Railroader_Data\Managed` to your Reference Paths
+
+### Requires
++ 0Harmony.dll
++ UnityModManager.dll
---
+++ main.cs
@@ -0,0 +1,97 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using Game.Events;
+using Network;
+using UnityEngine;
+using UnityModManagerNet;
+using Debug = UnityEngine.Debug;
+
+namespace ServerWaitOnEmpty
+{
+    public class PlayerWatch
+    {
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            modEntry.OnToggle = OnToggle;
+            return true;
+        }
+
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool enabled)
+        {
+            if (enabled)
+            {
+                Messenger.Default.Register<PlayersDidChange>(modEntry, OnPlayersDidChange);
+                Messenger.Default.Register<TimeMinuteDidChange>(modEntry, OnTimeChanged);
+                Messenger.Default.Register<MapDidLoadEvent>(modEntry, OnMapDidLoad);
+                Debug.Log("Registered events");
+            } else
+            {
+                Messenger.Default.Unregister<PlayersDidChange>(modEntry, OnPlayersDidChange);
+               Messenger.Default.Unregister<TimeMinuteDidChange>(modEntry, OnTimeChanged);
+                Messenger.Default.Unregister<MapDidLoadEvent>(modEntry, OnMapDidLoad);
+                SetPaused(false);
+                AudioListener.volume = 1f;
+                Debug.Log("Unregistered events");
+            }
+
+            return true;
+        }
+
+        private static void OnPlayersDidChange(PlayersDidChange message)
+        {
+            Debug.Log("OnPlayersDidChange called");
+            CheckStatus();
+        }
+
+        private static void OnTimeChanged(TimeMinuteDidChange message)
+        {
+            Debug.Log("OnTimeChanged called");
+            CheckStatus();
+        }
+
+        private static void OnMapDidLoad(MapDidLoadEvent message)
+        {
+            Debug.Log("OnMapDidLoad called");
+            CheckStatus();
+        }
+
+        private static void CheckStatus()
+        {
+            if (Multiplayer.Mode != ConnectionMode.MultiplayerServer)
+            {
+                Debug.Log("Client is not server host");
+                return;
+            }
+
+            var manager = UnityEngine.Object.FindObjectOfType<Game.State.PlayersManager>();
+            int count = 0;
+            if (manager != null)
+            {
+                foreach (var p in manager.RemotePlayers)
+                {
+                    count++;
+                }
+                Debug.Log("Remote Players: " + count.ToString());
+            }
+
+            if (count <= 0)
+            {
+                SetPaused(true);
+                Debug.Log("No Players - Game Paused");
+                global::Console.Log("No Players - Game Paused");
+            }
+            else
+            {
+                SetPaused(false);
+                Debug.Log("Unpausing");
+                global::Console.Log("Unpausing");
+            }
+        }
+
+        private static void SetPaused(bool paused)
+        {
+            Time.timeScale = paused ? 0f : 1f;
+            AudioListener.volume = 0f;
+        }
+    }
+}
+
